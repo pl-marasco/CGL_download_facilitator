9a84789d261f4ce189b264efd43d47d52858df3f
Pier <PhenoloBoy@users.noreply.github.com>
2022-07-15 09:17:30 +0200

    reformat

--- cgls_land.py
+++ cgls_land.py
@@ -72,7 +72,7 @@ class Session(object):
             return
 
         int_product_list = pd.read_html(col_manifest_response.text, skiprows=2)[0]['Parent Directory'].dropna(
-         ).values[-1]
+        ).values[-1]
         purl = f'{url}{int_product_list}'
 
         try:
@@ -82,7 +82,7 @@ class Session(object):
             return
 
         rows = specific_manifest.text.split('\n')
-        observation_table = pd.DataFrame()
+        observations_table = pd.DataFrame()
 
         for n_index, line in enumerate(rows[:-1]):
             r = re.search(r"\d\d\d\d/\d\d/\d\d", line)
@@ -93,66 +93,66 @@ class Session(object):
             sensor = name_components[-2]
             version = name_components[-1][-5:]
 
-            if '-RT' in name:
-                RT = name_components[0][-3:]
-                observation_table = pd.concat(
-                    [observation_table, pd.DataFrame([[name, int_path, exploded[-1], line, sensor, version, RT,
-                                                       pd.to_datetime(r[0], format="%Y/%m/%d")]],
-                                                     columns=['name', 'int_path', 'file_name', 'url', 'sensor',
-                                                              'version', 'rt', 'date'],
-                                                     index=[n_index]
-                                                     )
+            if '-rt' in name:
+                rt = name_components[0][-3:]
+                observations_table = pd.concat(
+                    [observations_table, pd.dataframe([[name, int_path, exploded[-1], line, sensor, version, rt,
+                                                        pd.to_datetime(r[0], format="%y/%m/%d")]],
+                                                      columns=['name', 'int_path', 'file_name', 'url', 'sensor',
+                                                               'version', 'rt', 'date'],
+                                                      index=[n_index]
+                                                      )
                      ])
 
             else:
-                observation_table = pd.concat(
-                    [observation_table, pd.DataFrame([[name, int_path, exploded[-1], line, sensor, version,
-                                                       pd.to_datetime(r[0], format="%Y/%m/%d")]],
-                                                     columns=['name', 'int_path', 'file_name', 'url', 'sensor',
-                                                              'version', 'date'],
-                                                     index=[n_index]
-                                                     )
+                observations_table = pd.concat(
+                    [observations_table, pd.dataframe([[name, int_path, exploded[-1], line, sensor, version,
+                                                        pd.to_datetime(r[0], format="%y/%m/%d")]],
+                                                      columns=['name', 'int_path', 'file_name', 'url', 'sensor',
+                                                               'version', 'date'],
+                                                      index=[n_index]
+                                                      )
                      ])
 
             if '-RT' in name:
-                observation_table = observation_table.sort_values(by=['date', 'rt'], ascending=True).reset_index(
+                observations_table = observations_table.sort_values(by=['date', 'rt'], ascending=True).reset_index(
                     drop=True)
             else:
-                observation_table = observation_table.sort_values(by=['date'], ascending=True).reset_index(drop=True)
+                observations_table = observations_table.sort_values(by=['date'], ascending=True).reset_index(drop=True)
 
-        return Collection(product_name, observation_table, self.session)
+        return Collection(product_name, observations_table, self.session)
 
 
 class Collection:
 
-    def __init__(self, name, observation_table, session):
+    def __init__(self, name, observations_table, session):
         self.session = session
         self.name = name
-        self.observation_table = observation_table
+        self.observations_table = observations_table
         self.sensors = self.set_sensors()
         self.start_date = None
         self.end_date = None
         self.set_date_range()
         self.sensors = self.set_sensors()
         self.alg_version = self.set_alg_version_list()
-        if 'rt' in self.observation_table.columns:
-            self.rt = self.observation_table['rt'].unique()
+        if 'rt' in self.observations_table.columns:
+            self.rt = self.observations_table['rt'].unique()
         else:
             self.rt = None
         self.path = None
 
     def set_date_range(self):
-        self.start_date = self.observation_table['date'].head(1).item()
-        self.end_date = self.observation_table['date'].tail(1).item()
+        self.start_date = self.observations_table['date'].head(1).item()
+        self.end_date = self.observations_table['date'].tail(1).item()
 
     def set_alg_version_list(self):
-        return self.observation_table['version'].unique()
+        return self.observations_table['version'].unique()
 
     def set_rt(self):
-        return self.observation_table['rt'].unique()
+        return self.observations_table['rt'].unique()
 
     def set_sensors(self):
-        return self.observation_table['sensor'].unique()
+        return self.observations_table['sensor'].unique()
 
     @property
     def infos(self):
@@ -166,7 +166,7 @@ RT list          : {self.rt}''')
     def _select_date(self, date):
         download_list = []
         if date is None:
-            link, filename, int_path = self.observation_table.tail(1).item()[['url', 'file_name', 'int_path']]
+            link, filename, int_path = self.observations_table.tail(1).item()[['url', 'file_name', 'int_path']]
             download_list.append([link, filename, int_path])
         elif isinstance(date, str):
             req_date = pd.to_datetime(date)
@@ -174,8 +174,8 @@ RT list          : {self.rt}''')
             if not self.start_date <= req_date <= self.end_date:
                 raise Exception('Date are out of the valid range')
 
-            i = self.observation_table.index.get_indexer([req_date], method='ffill')[0]
-            download_list.append(self.observation_table.iloc[i][['url', 'file_name', 'int_path']].values.tolist())
+            i = self.observations_table.index.get_indexer([req_date], method='ffill')[0]
+            download_list.append(self.observations_table.iloc[i][['url', 'file_name', 'int_path']].values.tolist())
         elif isinstance(date, list):
             for n_date in date:
                 req_date = pd.to_datetime(n_date)
@@ -183,8 +183,8 @@ RT list          : {self.rt}''')
                 if not self.start_date <= req_date <= self.end_date:
                     raise Exception('Date are out of the valid range')
 
-                i = self.observation_table.index.get_indexer([req_date], method='ffill')[0]
-                download_list.append(self.observation_table.iloc[i][['url', 'file_name', 'int_path']].values.tolist())
+                i = self.observations_table.index.get_indexer([req_date], method='ffill')[0]
+                download_list.append(self.observations_table.iloc[i][['url', 'file_name', 'int_path']].values.tolist())
         elif isinstance(date, slice):
 
             assert (self.start_date <= date.start <= self.end_date) or \
@@ -192,17 +192,17 @@ RT list          : {self.rt}''')
             assert date.start <= date.stop, 'End date is before the start date'
             assert date.start != date.stop, 'Dates coincide, please use only one date'
 
-            i_start = self.observation_table.index.get_indexer([date.start], method='ffill')[0]
-            i_end   = self.observation_table.index.get_indexer([date.stop],  method='ffill')[0]
+            i_start = self.observations_table.index.get_indexer([date.start], method='ffill')[0]
+            i_end = self.observations_table.index.get_indexer([date.stop], method='ffill')[0]
 
-            download_list = self.observation_table.iloc[i_start:i_end+1][['url', 'file_name',
-                                                                         'int_path']].values.tolist()
+            download_list = self.observations_table.iloc[i_start:i_end + 1][['url', 'file_name',
+                                                                             'int_path']].values.tolist()
 
         return download_list
 
     # def _sel_sensor(self, sensor=None):
     #     if sensor is not None and sensor in self.sensors:
-    #         self.observation_table = self.observation_table[self.observation_table['sensor'] == sensor]
+    #         self.observations_table = self.observations_table[self.observations_table['sensor'] == sensor]
 
     def _path_constructor(self, path, download_list):
         self.path = path
@@ -216,10 +216,10 @@ RT list          : {self.rt}''')
             os.makedirs(self.path, mode=0o777, exist_ok=True)
 
     def _RT(self, rt=None):
-        if rt is not None and rt in self.observation_table.columns:
-            self.observation_table = self.observation_table[self.observation_table.RT == rt]
-        elif rt in self.observation_table.columns:
-            self.observation_table.drop_duplicates(subset=['date'], keep='last', inplace=True)
+        if rt is not None and rt in self.observations_table.columns:
+            self.observations_table = self.observations_table[self.observations_table.RT == rt]
+        elif rt in self.observations_table.columns:
+            self.observations_table.drop_duplicates(subset=['date'], keep='last', inplace=True)
         else:
             pass
 
@@ -242,14 +242,14 @@ RT list          : {self.rt}''')
                     q += f'version == "{alg}"'
                 else:
                     q += f'& version == "{alg}"'
-            self.observation_table = self.observation_table.query(q)
+            self.observations_table = self.observations_table.query(q)
 
         # calc date span
         if self.start_date is None or self.end_date is None:
             _ = self.set_date_range()
         # check and change index to dataindex
-        if isinstance(self.observation_table.index[0], np.int64):
-            self.observation_table.set_index(['date'], inplace=True, drop=True)
+        if isinstance(self.observations_table.index[0], np.int64):
+            self.observations_table.set_index(['date'], inplace=True, drop=True)
 
         # select downloadable files by date
         download_list = self._select_date(date)
@@ -308,5 +308,3 @@ RT list          : {self.rt}''')
             return Exception((file_name, "Size incompatible with the original"))
 
         return d_path
-
-
